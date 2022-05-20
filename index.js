c449665191ec97d6dbd74ead572c2f87ebbdce9a
Carlos Ramirez <karlossander92@gmail.com>
2022-05-20 17:12:15 -0500

    Video 392

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import router from './routes/index.js';
+
+const app = express();
+
+// Definir puerto
+const port = process.env.PORT || 4000;
+
+// Habilitar PUG
+app.set('view engine', 'pug');
+
+// Definir la carpeta publica
+app.use(express.static('public'));
+
+// Agregar Route
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+})
+
+
